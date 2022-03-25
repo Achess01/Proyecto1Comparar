f3b628e4f4bd8a6c6c0022ccc0a4f1763b7b91a1
Achess <jealtzo26@gmail.com>
2022-03-24 21:15:22 -0600

    First commit. Server

---
+++ Server/src/main/java/com/achess/server/MainTest.java
@@ -0,0 +1,35 @@
+/*
+ * Welcome
+ */
+package com.achess.server;
+
+import com.achess.server.lexer.JavaLexer;
+import com.achess.server.parser.JavaParser;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/**
+ *
+ * @author achess
+ */
+public class MainTest {
+     public static void main(String[] args) {                        
+        try{
+            FileInputStream stream = new FileInputStream("/home/achess/Documents/Tareas/Compi1/Proyecto1/JavaParserLexer/Test.java");
+            Reader reader = new InputStreamReader(stream);            
+            JavaLexer lexer = new JavaLexer(reader);            
+            JavaParser parser = new JavaParser(lexer);            
+            try{                
+                parser.parse();
+            }
+            catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        catch(FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
